3b1cd507d7172bacb630e4918e51ae229fae7cee
MarcelSussai <marcelsussaideoliveira@hotmail.com>
2021-05-03 00:20:24 -0300

    029 - terminando o slider

--- components/oquefacoSlider.js
+++ components/oquefacoSlider.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 const OQueFacoSlider = () => {
 
@@ -13,6 +13,8 @@ const OQueFacoSlider = () => {
   const [state_item_2, setState_item_2]   = useState(true);
   const [state_item_3, setState_item_3]   = useState(true);
   const [state_item_4, setState_item_4]   = useState(true);
+  
+  const [ani_1, setAni_1]   = useState(Boolean);
 
   const handle_click_slider_1 = (e) => {
     e.preventDefault();
@@ -30,10 +32,85 @@ const OQueFacoSlider = () => {
     e.preventDefault();
     ref_Art_Slider_Container.current.scrollLeft = ref_item_Slider_1.current.scrollWidth * 3;
   }
+  const handle_click_slider_prev = (e) => {
+    e.preventDefault();
+    if (state_item_1) {
+      ref_Art_Slider_Container.current.scrollLeft = ref_item_Slider_1.current.scrollWidth * 3;
+    } else {
+      ref_Art_Slider_Container.current.scrollLeft -= ref_item_Slider_1.current.scrollWidth;
+    }
+  };
+  const handle_click_slider_next = (e) => {
+    e.preventDefault();
+    if (state_item_4) {
+      ref_Art_Slider_Container.current.scrollLeft = 0;
+    } else {
+      ref_Art_Slider_Container.current.scrollLeft += ref_item_Slider_1.current.scrollWidth;
+    }
+  };
+
+  useEffect(() => {
+    const scroll = (e) => {
+      e.preventDefault();
+      let onAni = window.pageYOffset + (window.innerHeight * 3 / 4) - 32;
+
+      let Calc =  ref_Art_Slider_Container.current.offsetParent.offsetTop + 
+                  ref_Art_Slider_Container.current.offsetParent.offsetParent.offsetTop + 48;
+
+      let condition_1 = onAni >= Calc;
+      console.log(condition_1);
+      setAni_1(condition_1);
+    }
+
+    const scrollPosition = (e) => {
+      e.preventDefault();
+
+      let width1 = 0;
+      let width2 = ref_item_Slider_1.current.scrollWidth;
+      let width3 = ref_item_Slider_1.current.scrollWidth * 2;
+      let width4 = ref_item_Slider_1.current.scrollWidth * 3;
+      let refContainerLeft = ref_Art_Slider_Container.current.scrollLeft;
+
+      
+      if(refContainerLeft <= width1) {
+        setState_item_1(true);
+        setState_item_2(false);
+        setState_item_3(false);
+        setState_item_4(false);
+      } else
+      if(refContainerLeft <= width2) {
+        setState_item_1(false);
+        setState_item_2(true);
+        setState_item_3(false);
+        setState_item_4(false);
+      } else
+      if(refContainerLeft <= width3) {
+        setState_item_1(false);
+        setState_item_2(false);
+        setState_item_3(true);
+        setState_item_4(false);
+      } else
+      if(refContainerLeft <= width4) {
+        setState_item_1(false);
+        setState_item_2(false);
+        setState_item_3(false);
+        setState_item_4(true);
+      }
+
+    }
+
+    window.addEventListener("scroll", scroll, false);
+    ref_Art_Slider_Container.current.addEventListener('scroll', scrollPosition, false);
+    return () => {
+      window.removeEventListener("scroll", scroll, false);
+      ref_Art_Slider_Container.current.removeEventListener('scroll', scrollPosition, false)
+    };
+  }, [])
+
 
   return (
   <>
-    <div className="c-oquefaco-slider">
+    <div className={`c-oquefaco-slider ${ani_1 ? 'ani-appearByTop-03' : 'ani-appearByTopReversed-02'}`}>
 
       <article className="c-art-slider" ref={ref_Art_Slider_Container}>
 
@@ -115,14 +192,14 @@ const OQueFacoSlider = () => {
       </article>
 
       <div className="c-slider-nav">
-        <a href="" className="slider-btn-prev slider-btns-prevnext" > &lt; </a>
+        <a className="slider-btn-prev slider-btns-prevnext" onClick={handle_click_slider_prev}>&lt;</a>
         <div className="c-slider-btns-items">
-          <a href="" className="slider-btn-item">   </a>
-          <a href="" className="slider-btn-item">   </a>
-          <a href="" className="slider-btn-item">   </a>
-          <a href="" className="slider-btn-item">   </a>
+          <a className={`slider-btn-item ${state_item_1 ? 'slider-btn-item-mark' : ''}`} onClick={handle_click_slider_1}>   </a>
+          <a className={`slider-btn-item ${state_item_2 ? 'slider-btn-item-mark' : ''}`} onClick={handle_click_slider_2}>   </a>
+          <a className={`slider-btn-item ${state_item_3 ? 'slider-btn-item-mark' : ''}`} onClick={handle_click_slider_3}>   </a>
+          <a className={`slider-btn-item ${state_item_4 ? 'slider-btn-item-mark' : ''}`} onClick={handle_click_slider_4}>   </a>
         </div>
-        <a href="" className="slider-btn-next slider-btns-prevnext" > &gt; </a>
+        <a className="slider-btn-next slider-btns-prevnext" onClick={handle_click_slider_next}> &gt; </a>
       </div>
 
     </div>
